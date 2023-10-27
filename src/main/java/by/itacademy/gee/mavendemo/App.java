e488be62b13a6496eb09c3091057758eb7caaa53
Vadim Krasnov <2vadimkrasnov@gmail.com>
2023-10-27 15:10:30 +0300

    maven initial

---
+++ src/main/java/by/itacademy/gee/mavendemo/App.java
@@ -0,0 +1,13 @@
+package by.itacademy.gee.mavendemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
