0dd1bca6f080e5b994c22123ea08b3ad835d032a
Vadim Krasnov <2vadimkrasnov@gmail.com>
2023-10-29 20:00:30 +0300

    04-jacoco-report

--- src/test/java/by/itacademy/gee/mavendemo/AppTest.java
+++ src/test/java/by/itacademy/gee/mavendemo/AppTest.java
@@ -1,20 +1,34 @@
 package by.itacademy.gee.mavendemo;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 /**
- * Unit test for simple App.
+ * Unit test for App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void sumAPosit_vBPosit_v() {
+//        given
+        int a = 15;
+        int b = 10;
+        int expectedSum = 25;
+//        when
+        int actualSum = App.sum(a,b);
+//        then
+        assertEquals(expectedSum, actualSum);
+    }
+    @Test
+    public void sumAPosit_vMaximalBPosit_v() {
+//        given
+        int a = Integer.MAX_VALUE;
+        int b = 10;
+        int expectedSum = -2147483639;
+//        when
+        int actualSum = App.sum(a,b);
+//        then
+        assertEquals(expectedSum, actualSum);
     }
 }
