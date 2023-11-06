43b1531f79edb18cf90ef066a31cf935d452a27d
Vadim Krasnov <2vadimkrasnov@gmail.com>
2023-11-06 16:42:09 +0300

    Trying to push

--- src/main/java/by/itacademy/gee/mavendemo/App.java
+++ src/main/java/by/itacademy/gee/mavendemo/App.java
@@ -8,9 +8,14 @@ public class App
 {
     public static void main( String[] args ){
         System.out.println(sum(3,4));
+        System.out.println("Trying to push");
     }
+
+
 
     public static int sum(int a, int b) {
         return a+b;
     }
+
+
 }
